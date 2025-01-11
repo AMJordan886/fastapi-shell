9d3451c0d9b14ddf658391ee8de40458133ebb67
AMJordan886 <alvaromartinezjordan@yahoo.com>
2025-01-11 14:50:15 +0100

    commit

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
-from typing import Union
+from typing import Union, List
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
 
 app = FastAPI()
+
+items = []
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+
+
+
+
+@app.post("/items/")
+def create_item(item: Item):
+    items.append(item)
+    return item
+
+
+@app.get("/items/", response_model=List[Item])
+def get_items():
+    return items
+
+
+
+
+
+
 
 
 @app.get("/")
@@ -13,4 +46,6 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
 
